60bb711d7db586eea75c3fddeab27eacc8d48ed8
chuckaspeer <chuckaspeer@gmail.com>
2018-06-27 19:33:45 -0500

    Auth

--- index.js
+++ index.js
@@ -1,8 +1,70 @@
-const express = require('express');
-const session = require('express-session');
-
+require("dotenv").config();
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const Auth0Strategy = require("passport-auth0");
+const students = require("./students.json");
 const app = express();
 
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000000
+    }
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-const port = 3000;
-app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
+passport.use(
+  new Auth0Strategy(
+    {
+      domain: process.env.DOMAIN,
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: "/login",
+      scope: "openid email profile"
+    },
+    function(accessToken, refreshToken, extraParams, profile, done) {
+      return done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  return done(null, user);
+});
+passport.deserializeUser((user, done) => {
+  return done(null, user);
+});
+
+app.get(
+  "/login",
+  passport.authenticate("auth0", {
+    successRedirect: "/me",
+    failureRedirect: "/github",
+    failureFlash: true
+  })
+);
+function authenticated(req, res, next) {
+  if (req.user) {
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+}
+app.get("/me", (req, res, next) => {
+  if (!req.user){
+    res.status(500).json({message: "wHAT THE FUCK"});
+  }else{
+  res.status(200).send(students);
+  }
+});
+
+const port = 3001;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
